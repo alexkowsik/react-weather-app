53a3ac7de26ec051bd4302e1be89602deb21a5ca
Alexander Kowsik <alexander.kowsik@gmx.de>
2019-09-16 17:56:32 +0200

    Refactoring

--- src/components/CityInput.js
+++ src/components/CityInput.js
@@ -2,47 +2,46 @@ import React from 'react';
 import './CityInput.css';
 
 export default class CityInput extends React.Component {
-    render(props) {
-        const onKlickHandler = async e => {
-            e.persist();
-            const eventKey = e.which ? e.which : e.keyCode;
-            const city = e.target.value;
+  render(props) {
+    const onKlickHandler = async e => {
+      e.persist();
+      const eventKey = e.which ? e.which : e.keyCode;
+      const city = e.target.value;
 
-            // check if input contains only letters after Enter was pressed
-            if (eventKey === 13) {
-                if (/^[a-zA-ZäöüÄÖÜß ]+$/.test(city)) {
-                    e.target.classList.add('loading');
+      // check if input contains only letters after Enter was pressed
+      if (eventKey === 13) {
+        if (/^[a-zA-ZäöüÄÖÜß ]+$/.test(city)) {
+          e.target.classList.add('loading');
 
-                    if (await this.props.makeApiCall(city))
-                        e.target.placeholder = 'Enter a City...';
-                    else e.target.placeholder = 'City was not found, try again...';
-                } else e.target.placeholder = 'Please enter a valid city name...';
-                e.target.classList.remove('loading');
-                e.target.value = '';
-            }
-        };
+          if (await this.props.makeApiCall(city)) e.target.placeholder = 'Enter a City...';
+          else e.target.placeholder = 'City was not found, try again...';
+        } else e.target.placeholder = 'Please enter a valid city name...';
+        e.target.classList.remove('loading');
+        e.target.value = '';
+      }
+    };
 
-        const style = {
-            top: this.props.city ? '-380px' : '80px',
-            width: '600px',
-            display: 'inline-block',
-            padding: '10px 0px 10px 30px',
-            lineHeight: '120%',
-            position: 'relative',
-            borderRadius: '20px',
-            outline: 'none',
-            fontSize: '20px',
-            transition: 'all 0.5s ease-out'
-        };
+    const style = {
+      top: this.props.city ? '-380px' : '80px',
+      width: '600px',
+      display: 'inline-block',
+      padding: '10px 0px 10px 30px',
+      lineHeight: '120%',
+      position: 'relative',
+      borderRadius: '20px',
+      outline: 'none',
+      fontSize: '20px',
+      transition: 'all 0.5s ease-out'
+    };
 
-        return (
-            <input
-                className='city-input'
-                style={style}
-                type='text'
-                placeholder='Enter a City...'
-                onKeyPress={onKlickHandler}
-            />
-        );
-    }
+    return (
+      <input
+        className='city-input'
+        style={style}
+        type='text'
+        placeholder='Enter a City...'
+        onKeyPress={onKlickHandler}
+      />
+    );
+  }
 }
--- src/components/MainWeatherWindow.js
+++ src/components/MainWeatherWindow.js
@@ -2,45 +2,44 @@ import React from 'react';
 import './MainWeatherWindow.css';
 
 export default class MainWeatherWindow extends React.Component {
-    render(props) {
-        const Title = this.props.city ? null : <h1 className='title'>React Weather App</h1>;
+  render(props) {
+    const Title = this.props.city ? null : <h1 className='title'>React Weather App</h1>;
 
-        return (
-            <div className='main'>
-                <div className='inner-main'>
-                    {Title}
-                    <img
-                        src={
-                            this.props.data
-                                ? require(`../images/${this.props.data.icon}.svg`)
-                                : require('../images/01d.svg')
-                        }
-                        alt='sun'
-                        style={{
-                            visibility: this.props.city ? 'visible' : 'hidden',
-                            opacity: this.props.city ? '1' : '0'
-                        }}
-                    />
+    return (
+      <div className='main'>
+        <div className='inner-main'>
+          {Title}
+          <img
+            src={
+              this.props.data
+                ? require(`../images/${this.props.data.icon}.svg`)
+                : require('../images/01d.svg')
+            }
+            alt='sun'
+            style={{
+              visibility: this.props.city ? 'visible' : 'hidden',
+              opacity: this.props.city ? '1' : '0'
+            }}
+          />
 
-                    <div
-                        className='today'
-                        style={{
-                            visibility: this.props.city ? 'visible' : 'hidden',
-                            opacity: this.props.city ? '1' : '0'
-                        }}
-                    >
-                        <span>Today</span>
-                        <h1>{this.props.city}</h1>
-                        <p>
-                            Temperature:{' '}
-                            {this.props.data ? Math.round(this.props.data.temp - 273.15) : 0}
-                            °C
-                        </p>
-                        <p>{this.props.data ? this.props.data.weather_desc.toLowerCase() : ''}</p>
-                    </div>
-                </div>
-                {this.props.children}
-            </div>
-        );
-    }
+          <div
+            className='today'
+            style={{
+              visibility: this.props.city ? 'visible' : 'hidden',
+              opacity: this.props.city ? '1' : '0'
+            }}
+          >
+            <span>Today</span>
+            <h1>{this.props.city}</h1>
+            <p>
+              Temperature: {this.props.data ? Math.round(this.props.data.temp - 273.15) : 0}
+              °C
+            </p>
+            <p>{this.props.data ? this.props.data.weather_desc.toLowerCase() : ''}</p>
+          </div>
+        </div>
+        {this.props.children}
+      </div>
+    );
+  }
 }
